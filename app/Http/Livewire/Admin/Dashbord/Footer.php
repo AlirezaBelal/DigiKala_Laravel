b9d11462fb482d459e7ecb0113dc5335d03750de
Alireza Belal <belal.alireza@yahoo.com>
2021-10-02 13:01:01 +0330

    add livewire footer

---
+++ app/Http/Livewire/Admin/Dashbord/Footer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Dashbord;
+
+use Livewire\Component;
+
+class Footer extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.dashbord.footer');
+    }
+}
---
+++ resources/views/livewire/admin/dashbord/footer.blade.php
@@ -0,0 +1,3 @@
+<script src="{{asset('js/jquery-3.4.1.min.js')}}"></script>
+<script src="{{'js/js.js'}}"></script>
+<livewire:scripts/>
