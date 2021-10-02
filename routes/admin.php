38d433222655bcba4349e63a449b98bfb8c4f377
Alireza Belal <belal.alireza@yahoo.com>
2021-10-02 16:07:46 +0330

    add route category

--- routes/admin.php
+++ routes/admin.php
@@ -13,4 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',\App\Http\Livewire\Admin\Dashbord\Index::class);
+Route::get('/',\App\Http\Livewire\Admin\Dashbord\Index::class)
+    ->name('admin.index');
+
+Route::get('/category' , \App\Http\Livewire\Admin\Category\Index::class)
+    ->name('category.index');
