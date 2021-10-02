1b37c1f52be88027bd159e4cc09d38e36ad69a77
Alireza Belal <belal.alireza@yahoo.com>
2021-10-02 14:19:12 +0330

    edit logo

--- resources/views/livewire/admin/dashbord/header.blade.php
+++ resources/views/livewire/admin/dashbord/header.blade.php
@@ -1,7 +1,7 @@
 <div class="header d-flex item-center bg-white width-100 border-bottom padding-12-30">
     <div class="header__right d-flex flex-grow-1 item-center">
         <span class="bars"></span>
-        <a class="header__logo" href=""></a>
+        <a href="/"><img style="height:44px;margin-left: 20px;" src="{{asset('img/weblogo.png')}}"></a>
     </div>
     <div class="header__left d-flex flex-end item-center margin-top-2">
         <span class="account-balance font-size-12">موجودی : 2500,000 تومان</span>
