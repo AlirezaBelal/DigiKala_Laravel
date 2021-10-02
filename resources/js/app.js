e7061f9b2dbb8ef1f2249638b94980ae392b2906
Alireza Belal <belal.alireza@yahoo.com>
2021-10-02 20:34:23 +0330

    Added sweetalert2 files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
 require('./bootstrap');
+
+
+
+let Swa = require('sweetalert2');
+
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
+
+  Toast.fire({
+    icon: 'success',
+    title: 'Signed in successfully'
+  })
