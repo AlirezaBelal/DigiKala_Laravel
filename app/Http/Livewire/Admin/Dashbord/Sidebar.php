8818b4a1d43023d5e837dc495acdedbd0bb6e26e
Alireza Belal <belal.alireza@yahoo.com>
2021-10-02 13:01:54 +0330

    add livewire sidebar

---
+++ app/Http/Livewire/Admin/Dashbord/Sidebar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Dashbord;
+
+use Livewire\Component;
+
+class Sidebar extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.dashbord.sidebar');
+    }
+}
