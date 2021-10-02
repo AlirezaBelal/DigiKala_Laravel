b97fbd479e32ee2ff253f02c099ff1f5c3c4480a
Alireza Belal <belal.alireza@yahoo.com>
2021-10-02 13:01:31 +0330

    add livewire head

---
+++ app/Http/Livewire/Admin/Dashbord/Head.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Dashbord;
+
+use Livewire\Component;
+
+class Head extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.dashbord.head');
+    }
+}
---
+++ resources/views/livewire/admin/dashbord/head.blade.php
@@ -0,0 +1,20 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+<title>
+    دیجی کالا |
+    @yield('title')
+</title>
+
+<link rel="stylesheet" href="{{asset('css/style.css')}}">
+
+<link rel="stylesheet" href="{{asset('css/responsive_991.css')}}" media="(max-width:991px)">
+
+<link rel="stylesheet" href="{{asset('css/responsive_768.css')}}" media="(max-width:768px)">
+
+<link rel="stylesheet" href="{{asset('css/font.css')}}">
+
+<livewire:styles/>
+
