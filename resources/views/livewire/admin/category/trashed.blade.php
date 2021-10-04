ba01179fd16f972b842f6101dd8289dd2d19c3b1
Alireza Belal <belal.alireza@yahoo.com>
2021-10-05 00:53:31 +0330

    edit view category

--- resources/views/livewire/admin/category/trashed.blade.php
+++ resources/views/livewire/admin/category/trashed.blade.php
@@ -1,78 +1,99 @@
-@section('title' , 'سطل زباله دسته ها')
+@section('title','سطل زباله دسته ها')
 
 <div>
     <div class="main-content" wire:init="loadCategory">
         <div class="tab__box">
             <div class="tab__items">
-                <a class="tab__item {{Request::routeIs('category.index') ? 'is-active' : ''}}" href="/admin/category">دسته
-                    ها</a>
-                <a class="tab__item {{Request::routeIs('subcategory.index') ? 'is-active' : ''}}"
-                   href="/admin/subcategory">زیردسته ها</a>
-                <a class="tab__item {{Request::routeIs('childcategory.index') ? 'is-active' : ''}}"
-                   href="/admin/childcategory">دسته های کودک</a>
 
+                {{--/admin/category--}}
+                <a class="tab__item is-active" href="{{route('category.index')}}">
+                    دسته ها
+                </a>
+
+                {{--/admin/subcategory--}}
+                <a class="tab__item {{Request::routeIs('subcategory.index') ? 'is-active': '' }}"
+                   href="{{route('subcategory.index')}}">
+                    زیر دسته ها
+                </a>
+
+                {{--/admin/childcategory--}}
+                <a class="tab__item {{Request::routeIs('childcategory.index') ? 'is-active': '' }}"
+                   href="{{route('childcategory.index')}}">
+                    دسته های کودک
+                </a>
                 |
-
-                <a class="tab__item">جستجو دسته ها</a>
+                <a class="tab__item">
+                    جستجو:
+                </a>
 
                 <a class="t-header-search">
                     <form action="" onclick="event.preventDefault();">
-                        <input wire:model.debounce.1000="search"
-                               type="text" class="text" placeholder="جستجوی دسته ... ">
+                        <input type="text" class="text" placeholder="جستجوی دسته ..."
+                               wire:model.debounce.1000="search">
                     </form>
                 </a>
 
                 <a class="tab__item btn btn-danger"
                    href="{{route('category.trashed')}}"
-                   style="color: white;float: left; margin-top: 10px;margin-left: 10px">
+                   style="color: white;float: left;margin-top: 10px;margin-left: 10px">
                     سطل زباله
                     ({{\App\Models\Category::onlyTrashed()->count()}})
                 </a>
             </div>
-
         </div>
 
         <div class="row">
             <div class="col-12 margin-left-10 margin-bottom-15 border-radius-3">
-
                 <div class="table__box">
-                    <table class="table">
 
+                    <table class="table">
                         <thead role="rowgroup">
                         <tr role="row" class="title-row">
-                            <th>شناسه آیدی</th>
+                            <th>آیدی</th>
                             <th>تصویر دسته</th>
-                            <th>نام دسته</th>
                             <th>عنوان دسته</th>
+                            <th>نام دسته</th>
                             <th>عملیات</th>
                         </tr>
                         </thead>
 
-                        @if($readToLoad)
+                        @if($readyToLoad)
                             <tbody>
                             @foreach($categories as $category)
                                 <tr role="row">
-                                    <td><a href="">{{$category->id}}</a></td>
                                     <td>
-                                        <img src="/storage/{{$category->img}}" width="50px">
+                                        <a href="">{{$category->id}}</a>
                                     </td>
-                                    <td><a href="">{{$category->title}}</a></td>
-                                    <td><a href="">{{$category->name}}</a></td>
-
 
                                     <td>
-                                        <a wire:click="deleteCategory({{$category->id}})"
-                                            type="submit" class="item-delete mlg-15" title="حذف"></a>
+                                        <img src="/storage/{{$category->img}}" alt="img" width="100px">
+                                    </td>
 
-                                        <a wire:clik="trashedCategory({{$category->id}})"
-                                           class="item-li i-checkouts item-restore" href=""> </a>
+                                    <td>
+                                        <a href="">{{$category->title}}</a>
+                                    </td>
 
+                                    <td>
+                                        <a href="">{{$category->name}}</a>
+                                    </td>
+
+                                    <td>
+                                        <a type="submit" class="item-delete mlg-15"
+                                           wire:click="deleteCategory({{$category->id}})"
+                                           title="حذف">
+                                        </a>
+
+                                        <a class="item-li i-checkouts item-restore"
+                                           wire:click="trashedCategory({{$category->id}})">
+                                        </a>
                                     </td>
                                 </tr>
                             @endforeach
+
                             </tbody>
 
                             {{$categories->render()}}
+
                         @else
                             <div class="alert-warning alert">
                                 در حال خواندن اطلاعات از دیتابیس ...
@@ -85,4 +106,3 @@
         </div>
     </div>
 </div>
-
--- resources/views/livewire/admin/category/update.blade.php
+++ resources/views/livewire/admin/category/update.blade.php
@@ -1,4 +1,4 @@
-@section('title' , 'ویرایش دسته')
+@section('title','آپدیت دسته')
 
 <div>
     <div class="main-content padding-0">
@@ -7,56 +7,56 @@
             -
             {{$category->title}}
         </p>
-        <div class="row no-gutters bg-white">
-            <div class="col-6">
-                <form wire:submit.prevent="categoryForm" class="padding-15"
-                      enctype="multipart/form-data" role="form">
 
+        <div class="row no-gutters bg-white">
+            <div class="col-8">
+                <form wire:submit.prevent="categoryForm" enctype="multipart/form-data" role="form" class="padding-10 categoryForm">
 
                     @include('errors.error')
+
                     <div class="form-group">
-                        <input type="text" wire:model.lazy="category.title" placeholder="نام دسته" class="form-control">
+                        <input type="text" placeholder="نام دسته " class="form-control"
+                               wire:model.lazy="category.title">
                     </div>
 
                     <div class="form-group">
-                        <input type="text" wire:model.lazy="category.name" placeholder="نام انگلیسی دسته"
-                               class="form-control">
+                        <input type="text" placeholder="نام انگلیسی دسته " class="form-control"
+                               wire:model.lazy="category.name">
                     </div>
 
                     <div class="form-group">
-                        <input type="text" wire:model.lazy="category.link" placeholder="لینک دسته" class="form-control">
+                        <input type="text" placeholder="لینک دسته " class="form-control"
+                               wire:model.lazy="category.link">
                     </div>
 
                     <div class="form-group">
                         <div class="notificationGroup">
-                            <input id="option4" type="checkbox" wire:model.lazy="category.status" name="status"
-                                   class="form-control">
-                            <label for="option4">نمایش در دسته اصلی:</label>
-                        </div>
+                            <input id="option4" type="checkbox" name="status" class="form-control"
+                                   wire:model.lazy="category.status">
 
+                            <label for="option4">
+                                نمایش در دسته اصلی:
+                            </label>
+                        </div>
                     </div>
 
                     <div class="form-group">
                         <input type="file" wire:model.lazy="img" class="form-control">
-                        <span class="m-2 text-danger" wire:loading wire:target="img">
-                            در حال آپلود ...
-                        </span>
-                        <div wire:ignore class="progress mt-3" id="progressbar" style="display: none">
+                        <span class="mt-2 text-danger" wire:loading wire:target="img">در حال آپلود ...</span>
+
+                        <div wire:ignore class="progress mt-2" id="progressbar" style="display: none">
                             <div class="progress-bar" role="progressbar" style="width: 0%;">0%</div>
                         </div>
                     </div>
 
                     <div>
                         @if($img)
-                            <img class="form-control mt-3" width="250px" src="{{$img->temporaryUrl()}}" alt="" style="width:200px">
+                            <img style="    width: 200px;" class="form-control mt-3 mb-3" width="400"
+                                 src="{{$img->temporaryUrl()}}" alt="">
                         @endif
                     </div>
-
-                    <button class="btn btn-brand mt-3">ویرایش دسته</button>
+                    <button type="submit" class="btn btn-brand">آپدیت دسته</button>
                 </form>
-            </div>
-            <div class="col-6>
-
             </div>
         </div>
     </div>
