d79e9f3680af925ecd580b2b26b04c61c4baa2c2
Alireza Belal <belal.alireza@yahoo.com>
2021-10-04 13:27:27 +0330

    edit route

--- routes/home.php
+++ routes/home.php
@@ -12,11 +12,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+
 
