2a39addd83029e4858b803ff63f5923e26a09e74
Alireza Belal <belal.alireza@yahoo.com>
2021-10-04 11:21:29 +0330

    Handle database subcategory

---
+++ database/migrations/2021_10_03_233031_create_sub_categories_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_categories', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('img')->nullable();
+            $table->string('title');
+            $table->string('parent');
+            $table->string('name')->nullable();
+            $table->string('link')->nullable();
+            $table->string('status')->default(1);
+
+            $table->softDeletes();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_categories');
+    }
+}
