8b4dae308ee1aa61584c6b0799552d2c80389578
Alireza Belal <belal.alireza@yahoo.com>
2021-10-06 20:58:51 +0330

    fix ckeditor

--- resources/views/livewire/admin/dashbord/head.blade.php
+++ resources/views/livewire/admin/dashbord/head.blade.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="{{asset('css/responsive_991.css')}}" media="(max-width:991px)">
     <link rel="stylesheet" href="{{asset('css/responsive_768.css')}}" media="(max-width:768px)">
     <link rel="stylesheet" href="{{asset('css/font.css')}}">
+    <script src="{{asset('js/ckeditor.js')}}"></script>
     <livewire:styles />
 
     <style>
