1b9904fab640cd29a8de07a831b5cbca3d414007
Alireza Belal <belal.alireza@yahoo.com>
2021-10-07 20:54:49 +0330

    database brand

---
+++ database/migrations/2021_10_07_193553_create_brands_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBrandsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name')->nullable();
+            $table->string('img')->nullable();
+            $table->text('description')->nullable();
+            $table->string('status')->default(1);
+            $table->string('parent');
+            $table->string('link')->nullable();
+
+            $table->softDeletes();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('brands');
+    }
+}
