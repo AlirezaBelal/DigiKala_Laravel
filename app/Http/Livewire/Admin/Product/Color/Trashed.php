2c8e90bb8cab827a0b7815fb0a0495854004e87e
Alireza Belal <belal.alireza@yahoo.com>
2021-10-07 23:37:19 +0330

    add update trashed by livewire

---
+++ app/Http/Livewire/Admin/Product/Color/Trashed.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Product\Color;
+
+use Livewire\Component;
+
+class Trashed extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.product.color.trashed');
+    }
+}
