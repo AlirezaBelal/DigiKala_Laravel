96d0d2d41f05196ea818732a129ffab3ecc74f74
Alireza Belal <belal.alireza@yahoo.com>
2021-10-08 10:08:55 +0330

    add jscolor

--- resources/views/livewire/admin/dashbord/footer.blade.php
+++ resources/views/livewire/admin/dashbord/footer.blade.php
@@ -1,4 +1,5 @@
 <script src="{{asset('js/jquery-3.4.1.min.js')}}"></script>
 <script src="{{asset('js/js.js')}}"></script>
+<script src="{{asset('js/jscolor.js')}}"></script>
 <livewire:scripts />
 
