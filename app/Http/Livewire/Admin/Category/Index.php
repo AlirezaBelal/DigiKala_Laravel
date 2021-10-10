5911f26278510f28cdd09361b25fefdeeffbb8c1
Alireza Belal <belal.alireza@yahoo.com>
2021-10-10 13:05:11 +0330

    Code Refractor category

---
+++ app/Http/Livewire/Admin/Category/Index.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Category;
+
+use App\Models\Category;
+use App\Models\Log;
+use App\Models\SubCategory;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+use Livewire\WithPagination;
+
+class Index extends Component
+{
+    use WithFileUploads;
+    use WithPagination;
+
+    public Category $category;
+    public $img;
+    public $search;
+    public $readyToLoad = false;
+
+    protected $queryString = ['search'];
+    /**
+     * @var string
+     * Site front type
+     */
+    protected $paginationTheme = 'bootstrap';
+
+
+    /**
+     * @var string[]
+     * Input rules
+     */
+    protected $rules = [
+        'category.title' => 'required|min:3',
+        'category.name' => 'required',
+        'category.link' => 'required',
+        'category.status' => 'nullable',
+    ];
+
+
+    public function mount()
+    {
+        $this->category = new Category();
+    }
+
+
+    /**
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function updated($title)
+    {
+        $this->validateOnly($title);
+    }
+
+
+    public function loadCategory()
+    {
+        $this->readyToLoad = true;
+    }
+
+
+    public function categoryForm()
+    {
+        $this->validate();
+
+        $category = Category::query()->create([
+            'title' => $this->category->title,
+            'name' => $this->category->name,
+            'link' => $this->category->link,
+            'status' => $this->category->status ? true : false,
+        ]);
+
+        if ($this->img) {
+            $category->update([
+                'img' => $this->uploadImage()
+            ]);
+        }
+
+        //Empty the form then fill out the form
+        $this->category->title = "";
+        $this->category->name = "";
+        $this->category->link = "";
+        $this->category->status = false;
+        $this->img = null;
+
+        //Create a report
+        Log::create([
+            'user_id' => auth()->user()->id,
+            'url' => 'افزودن دسته' . '-' . $this->category->title,
+            'actionType' => 'ایجاد'
+        ]);
+
+        $this->emit('toast', 'success', ' دسته با موفقیت ایجاد شد.');
+    }
+
+
+    /**
+     * @return string
+     * Upload image to memory
+     */
+    public function uploadImage()
+    {
+        $year = now()->year;
+        $month = now()->month;
+        $directory = "category/$year/$month";
+        $name = $this->img->getClientOriginalName();
+        $this->img->storeAs($directory, $name);
+        return "$directory/$name";
+    }
+
+
+    public function updateCategoryDisable($id)
+    {
+        $category = Category::find($id);
+        $category->update([
+            'status' => 0
+        ]);
+        Log::create([
+            'user_id' => auth()->user()->id,
+            'url' => 'غیرفعال کردن وضعیت دسته' . '-' . $category->title,
+            'actionType' => 'غیرفعال'
+        ]);
+        $this->emit('toast', 'success', 'وضعیت دسته با موفقیت غیرفعال شد.');
+    }
+
+
+    public function updateCategoryEnable($id)
+    {
+        $category = Category::find($id);
+        $category->update([
+            'status' => 1
+        ]);
+        Log::create([
+            'user_id' => auth()->user()->id,
+            'url' => 'فعال کردن وضعیت دسته' . '-' . $category->title,
+            'actionType' => 'فعال'
+        ]);
+        $this->emit('toast', 'success', 'وضعیت دسته با موفقیت فعال شد.');
+    }
+
+
+    public function deleteCategory($id)
+    {
+        $category = Category::find($id);
+        $subCategory = SubCategory::where('parent', $id)->first();
+        if ($subCategory == null) {
+            $category->delete();
+            Log::create([
+                'user_id' => auth()->user()->id,
+                'url' => 'حذف کردن دسته' . '-' . $category->title,
+                'actionType' => 'حذف'
+            ]);
+            $this->emit('toast', 'success', ' دسته با موفقیت حذف شد.');
+        } else {
+            $this->emit('toast', 'success', ' امکان حذف وجود ندارد زیرا زیردسته دارد!');
+        }
+
+    }
+
+
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     * Send information to the site and load the site
+     */
+    public function render()
+    {
+        $categories = $this->readyToLoad ? Category::where('title', 'LIKE', "%{$this->search}%")
+            ->orWhere('name', 'LIKE', "%{$this->search}%")
+            ->orWhere('link', 'LIKE', "%{$this->search}%")
+            ->latest()->paginate(10) : [];
+
+        return view('livewire.admin.category.index', compact('categories'));
+    }
+}
--- resources/views/livewire/admin/category/index.blade.php
+++ resources/views/livewire/admin/category/index.blade.php
@@ -4,28 +4,26 @@
     <div class="main-content" wire:init="loadCategory">
         <div class="tab__box">
             <div class="tab__items">
-
                 <a class="tab__item is-active" href="{{route('category.index')}}">
                     دسته ها
                 </a>
 
-                <a class="tab__item" href="{{route('subcategory.index')}}">
+                <a class="tab__item {{Request::routeIs('subcategory.index') ? 'is-active': '' }}"
+                   href="{{route('subcategory.index')}}">
                     زیر دسته ها
                 </a>
 
-                {{--{{Request::routeIs('childcategory.index') ? 'is-active': '' }}--}}
-                <a class="tab__item " href="{{route('childcategory.index')}}">
+                <a class="tab__item {{Request::routeIs('childcategory.index') ? 'is-active': '' }}"
+                   href="{{route('childcategory.index')}}">
                     دسته های کودک
                 </a>
                 |
-                <a class="tab__item">
-                    جستجو:
-                </a>
+                <a class="tab__item">جستجو: </a>
 
                 <a class="t-header-search">
                     <form action="" onclick="event.preventDefault();">
-                        <input type="text" class="text" placeholder="جستجوی دسته ..."
-                               wire:model.debounce.1000="search">
+                        <input wire:model.debounce.1000="search"
+                               type="text" class="text" placeholder="جستجوی دسته ...">
                     </form>
                 </a>
 
@@ -41,8 +39,8 @@
         <div class="row">
             <div class="col-8 margin-left-10 margin-bottom-15 border-radius-3">
                 <div class="table__box">
-
                     <table class="table">
+
                         <thead role="rowgroup">
                         <tr role="row" class="title-row">
                             <th>ردیف</th>
@@ -55,14 +53,13 @@
                         </thead>
 
                         @if($readyToLoad)
-                            <tbody>
                             @php($count = 1)
+                            <tbody>
                             @foreach($categories as $category)
                                 <tr role="row">
                                     <td>
-                                        <p>{{$count++}}<p>
+                                        {{$count++}}
                                     </td>
-
                                     <td>
                                         <img src="{{\Illuminate\Support\Facades\Storage::url($category->img)}}"
                                              alt="img" width="50px">
@@ -78,94 +75,83 @@
 
                                     <td>
                                         @if($category->status == 1)
-                                            <button type="submit" class="badge-success badge"
-                                                    style="background-color: green"
-                                                    wire:click="updateCategoryDisable({{$category->id}})">
+                                            <button wire:click="updateCategoryDisable({{$category->id}})"
+                                                    type="submit" class="badge-success badge"
+                                                    style="background-color: green">
                                                 فعال
                                             </button>
                                         @else
-                                            <button type="submit" class="badge-danger badge"
-                                                    style="background-color: red"
-                                                    wire:click="updateCategoryEnable({{$category->id}})">
+                                            <button wire:click="updateCategoryEnable({{$category->id}})"
+                                                    type="submit" class="badge-danger badge"
+                                                    style="background-color: red">
                                                 غیرفعال
                                             </button>
                                         @endif
                                     </td>
-
                                     <td>
-                                        <a type="submit" class="item-delete mlg-15"
-                                           wire:click="deleteCategory({{$category->id}})"
+                                        <a wire:click="deleteCategory({{$category->id}})" type="submit"
+                                           class="item-delete mlg-15"
                                            title="حذف">
                                         </a>
-
-                                        <a class="item-edit "
-                                           href="{{route('category.update',$category)}}"
+                                        <a href="{{route('category.update',$category)}}" class="item-edit"
                                            title="ویرایش">
                                         </a>
-
                                     </td>
                                 </tr>
                             @endforeach
                             </tbody>
                             {{$categories->render()}}
-
                         @else
                             <div class="alert-warning alert">
                                 در حال خواندن اطلاعات از دیتابیس ...
                             </div>
                         @endif
-
                     </table>
                 </div>
             </div>
 
             <div class="col-4 bg-white">
                 <p class="box__title">ایجاد دسته بندی جدید</p>
-
-                <form wire:submit.prevent="categoryForm" enctype="multipart/form-data" role="form"
+                <form wire:submit.prevent="categoryForm"
+                      enctype="multipart/form-data" role="form"
                       class="padding-10 categoryForm">
 
                     @include('errors.error')
 
                     <div class="form-group">
-                        <input type="text" placeholder="نام دسته " class="form-control"
-                               wire:model.lazy="category.title">
+                        <input type="text" wire:model.lazy="category.title" placeholder="نام دسته "
+                               class="form-control">
                     </div>
 
                     <div class="form-group">
-                        <input type="text" placeholder="نام انگلیسی دسته " class="form-control"
-                               wire:model.lazy="category.name">
+                        <input type="text" wire:model.lazy="category.name" placeholder="نام انگلیسی دسته "
+                               class="form-control">
                     </div>
 
                     <div class="form-group">
-                        <input type="text" placeholder="لینک دسته " class="form-control"
-                               wire:model.lazy="category.link">
+                        <input type="text" wire:model.lazy="category.link" placeholder="لینک دسته "
+                               class="form-control">
                     </div>
 
                     <div class="form-group">
                         <div class="notificationGroup">
-                            <input id="option4" type="checkbox" name="status" class="form-control"
-                                   wire:model.lazy="category.status">
-
-                            <label for="option4">
-                                نمایش در دسته اصلی:
-                            </label>
-
+                            <input id="option4" type="checkbox" wire:model.lazy="category.status" name="status"
+                                   class="form-control">
+                            <label for="option4">نمایش در دسته اصلی:</label>
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <input type="file" wire:model.lazy="img" class="form-control">
+                        <input type="file" wire:model.lazy="img" id="{{rand()}}" class="form-control">
                         <span class="mt-2 text-danger" wire:loading wire:target="img">در حال آپلود ...</span>
 
                         <div wire:ignore class="progress mt-2" id="progressbar" style="display: none">
                             <div class="progress-bar" role="progressbar" style="width: 0%;">0%</div>
                         </div>
                     </div>
-
                     <div>
                         @if($img)
-                            <img class="form-control mt-3 mb-3" width="400" src="{{$img->temporaryUrl()}}" alt="">
+                            <img class="form-control mt-3 mb-3" width="200" src="{{$img->temporaryUrl()}}" alt="">
                         @endif
                     </div>
                     <button class="btn btn-brand">افزودن دسته</button>
