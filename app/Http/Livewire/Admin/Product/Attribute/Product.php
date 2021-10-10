a762f14cee692b506e80c35dbb68e2bfebb8ae57
Alireza Belal <belal.alireza@yahoo.com>
2021-10-10 19:11:52 +0330

    Product features Product

---
+++ app/Http/Livewire/Admin/Product/Attribute/Product.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Product\Attribute;
+
+use App\Models\Attribute;
+use App\Models\AttributeValue;
+use App\Models\ChildCategory;
+use App\Models\Log;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class Product extends Component
+{
+    use WithPagination;
+
+
+    public AttributeValue $attribute;
+    public \App\Models\Product $product;
+    public $search;
+    public $readyToLoad = false;
+
+    protected $queryString = ['search'];
+    protected $paginationTheme = 'bootstrap';
+
+    protected $rules = [
+        'attribute.product_id' => 'nullable',
+        'attribute.attribute_id' => 'required',
+        'attribute.value' => 'required',
+        'attribute.status' => 'required',
+    ];
+
+
+    public function mount()
+    {
+        $this->attribute = new AttributeValue();
+    }
+
+
+    public function updated($product_id)
+    {
+        $this->validateOnly($product_id);
+    }
+
+
+    public function loadCategory()
+    {
+        $this->readyToLoad = true;
+    }
+
+
+    public function categoryForm()
+    {
+
+        $this->validate();
+        AttributeValue::query()->create([
+            'attribute_id' => $this->attribute->attribute_id,
+            'product_id' => $this->product->id,
+            'value' => $this->attribute->value,
+            'status' => $this->attribute->status ? 1 : 0,
+        ]);
+
+        $this->attribute->attribute_id = null;
+        $this->attribute->product_id = null;
+        $this->attribute->value = "";
+        $this->attribute->status = false;
+
+        Log::create([
+            'user_id' => auth()->user()->id,
+            'url' => 'افزودن مقدار مشخصه کالا' . '-' . $this->attribute->value,
+            'actionType' => 'ایجاد'
+        ]);
+
+        $this->emit('toast', 'success', ' مقدار مشخصه کالا با موفقیت ایجاد شد.');
+
+        return redirect()->back();
+    }
+
+
+    public function updateCategoryDisable($id)
+    {
+        $attribute = AttributeValue::find($id);
+        $attribute->update([
+            'status' => 0
+        ]);
+        Log::create([
+            'user_id' => auth()->user()->id,
+            'url' => 'غیرفعال کردن وضعیت مقدار مشخصه کالا' . '-' . $this->attribute->value,
+            'actionType' => 'غیرفعال'
+        ]);
+        $this->emit('toast', 'success', 'وضعیت مقدار مشخصه کالا با موفقیت غیرفعال شد.');
+    }
+
+
+    public function updateCategoryEnable($id)
+    {
+        $attribute = AttributeValue::find($id);
+        $attribute->update([
+            'status' => 1
+        ]);
+        Log::create([
+            'user_id' => auth()->user()->id,
+            'url' => 'فعال کردن وضعیت مقدار مشخصه کالا' . '-' . $this->attribute->value,
+            'actionType' => 'فعال'
+        ]);
+        $this->emit('toast', 'success', 'وضعیت مقدار مشخصه کالا با موفقیت فعال شد.');
+    }
+
+
+    public function deleteCategory($id)
+    {
+        $attribute = AttributeValue::find($id);
+        $attribute->delete();
+        Log::create([
+            'user_id' => auth()->user()->id,
+            'url' => 'حذف کردن مقدار مشخصه کالا' . '-' . $this->attribute->value,
+            'actionType' => 'حذف'
+        ]);
+        $this->emit('toast', 'success', ' مقدار مشخصه کالا با موفقیت حذف شد.');
+    }
+
+
+    public function render()
+    {
+
+        $product = $this->product;
+
+        $attributes = $this->readyToLoad ? AttributeValue::where('product_id', $this->product->id)
+            ->orderBy('product_id')->paginate(10) : [];
+
+        return view('livewire.admin.product.attribute.product', compact('attributes', 'product'));
+    }
+}
---
+++ resources/views/livewire/admin/product/attribute/product.blade.php
@@ -0,0 +1,129 @@
+@section('title','مقدار مشخصه کالا')
+
+<div>
+    <div class="main-content" wire:init="loadCategory">
+        <div class="tab__box">
+            <div class="tab__items">
+                <a class="tab__item ">
+                    مقدار مشخصه کالا -
+                    {{$this->product->title}}
+                </a>
+
+                <a class="tab__item btn btn-danger"
+                   href="{{route('attributeValue.trashed')}}"
+                   style="color: white;float: left;margin-top: 10px;margin-left: 10px">
+                    سطل زباله
+                    ({{\App\Models\AttributeValue::onlyTrashed()->count()}})
+                </a>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-8 margin-left-10 margin-bottom-15 border-radius-3">
+                <div class="table__box">
+                    <table class="table">
+
+                        <thead role="rowgroup">
+                        <tr role="row" class="title-row">
+                            <th>ردیف</th>
+                            <th>مشخصه کالا</th>
+                            <th>مقدار</th>
+                            <th>وضعیت</th>
+                            <th>عملیات</th>
+                        </tr>
+                        </thead>
+
+                        @if($readyToLoad)
+                            @php($count = 1)
+                            <tbody>
+                            @foreach($attributes as $attribute)
+                                <tr role="row">
+                                    <td>
+                                        {{$count++}}
+                                    </td>
+
+                                    <td>
+                                        @foreach(\App\Models\Attribute::where('id',$attribute->attribute_id)->get() as $att)
+                                            {{$att->title}}
+                                        @endforeach
+                                    </td>
+
+                                    <td>
+                                        {{$attribute->value}}
+                                    </td>
+
+                                    <td>
+                                        @if($attribute->status == 1)
+                                            <button wire:click="updateCategoryDisable({{$attribute->id}})"
+                                                    type="submit" class="badge-success badge"
+                                                    style="background-color: green">
+                                                فعال
+                                            </button>
+                                        @else
+                                            <button wire:click="updateCategoryEnable({{$attribute->id}})"
+                                                    type="submit" class="badge-danger badge"
+                                                    style="background-color: red">
+                                                غیرفعال
+                                            </button>
+                                        @endif
+                                    </td>
+
+                                    <td>
+                                        <a wire:click="deleteCategory({{$attribute->id}})" type="submit"
+                                           class="item-delete mlg-15"
+                                           title="حذف">
+                                        </a>
+                                        <a href="{{route('attributeValue.update',$attribute)}}" class="item-edit "
+                                           title="ویرایش">
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                            {{$attributes->render()}}
+                        @else
+                            <div class="alert-warning alert">
+                                در حال خواندن اطلاعات از دیتابیس ...
+                            </div>
+                        @endif
+                    </table>
+                </div>
+            </div>
+
+            <div class="col-4 bg-white">
+                <p class="box__title">ایجاد مشخصات فنی کالا</p>
+                <form wire:submit.prevent="categoryForm"
+                      enctype="multipart/form-data" role="form"
+                      class="padding-10 categoryForm">
+
+                    @include('errors.error')
+
+                    <div class="form-group">
+                        <select wire:model.lazy="attribute.attribute_id" name="attribute_id" id="" class="form-control">
+                            <option value="-1">- انتخاب مشخصه کالا -</option>
+                            @foreach(\App\Models\Attribute::where('parent','>', '0')
+                                ->where('childCategory',$this->product->childcategory_id)->get() as $attribute)
+                                <option value="{{$attribute->id}}">-- {{$attribute->title}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" wire:model.lazy="attribute.value" placeholder=" مقدار مشخصه کالا "
+                               class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <div class="notificationGroup">
+                            <input id="option4" type="checkbox" wire:model.lazy="attribute.status" name="status"
+                                   class="form-control">
+                            <label for="option4">نمایش در مقدار مشخصات کالا:</label>
+                        </div>
+                    </div>
+
+                    <button class="btn btn-brand">افزودن مقدار مشخصه کالا</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
