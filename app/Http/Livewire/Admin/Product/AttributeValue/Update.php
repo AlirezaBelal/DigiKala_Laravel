d8d169ed7345d11a5fba0429f0beed630e417cdf
Alireza Belal <belal.alireza@yahoo.com>
2021-10-10 20:06:30 +0330

    Product feature values update page

---
+++ app/Http/Livewire/Admin/Product/AttributeValue/Update.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Product\AttributeValue;
+
+use App\Http\Livewire\Admin\Product\Gallery\Product;
+use App\Models\Attribute;
+use App\Models\AttributeValue;
+use App\Models\Log;
+use Livewire\Component;
+
+class Update extends Component
+{
+    public AttributeValue $attribute;
+
+    protected $rules = [
+        'attribute.product_id' => 'required',
+        'attribute.attribute_id' => 'required',
+        'attribute.value' => 'required',
+        'attribute.status' => 'required',
+    ];
+
+
+    public function categoryForm()
+    {
+        $this->validate();
+        $this->attribute->update($this->validate());
+        if (!$this->attribute->status) {
+            $this->attribute->update([
+                'status' => 0
+            ]);
+        }
+
+        Log::create([
+            'user_id' => auth()->user()->id,
+            'url' => 'آپدیت مقدار مشخصه' .'-'. $this->attribute->title,
+            'actionType' => 'آپدیت'
+        ]);
+
+        alert()->success(' با موفقیت آپدیت شد.', 'مقدار مشخصه مورد نظر با موفقیت آپدیت شد.');
+        return redirect(route('attributeValue.index'));
+    }
+
+
+    public function render()
+    {
+        if ($this->attribute->status == 1){
+            $this->attribute->status = true;
+        }else
+        {
+            $this->attribute->status = false;
+        }
+        $attribute = $this->attribute;
+        $product = \App\Models\Product::find($attribute->product_id);
+        $att= \App\Models\Attribute::where('parent','>', '0') ->where('childCategory',$product->childcategory_id)->get();
+        return view('livewire.admin.product.attribute-value.update',
+            compact('attribute','product','att'));
+    }
+}
---
+++ resources/views/livewire/admin/product/attribute-value/update.blade.php
@@ -0,0 +1,54 @@
+@section('title','آپدیت مقدار مشخصه کالا')
+
+<div>
+    <div class="main-content padding-0">
+        <p class="box__title">
+            ویرایش مقدار مشخصه کالا -
+            {{$attribute->value}}
+        </p>
+
+        <div class="row no-gutters bg-white">
+            <div class="col-8">
+                <form wire:submit.prevent="categoryForm"
+                      enctype="multipart/form-data" role="form"
+                      class="padding-10 categoryForm">
+
+                    @include('errors.error')
+
+                    <div class="form-group">
+                        <select wire:model.lazy="attribute.product_id" name="product_id" id="" class="form-control">
+                            <option value="-1">- انتخاب محصول -</option>
+                            @foreach(\App\Models\Product::all() as $product)
+                                <option value="{{$product->id}}">{{$product->title}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <select wire:model.lazy="attribute.attribute_id" name="attribute_id" id="" class="form-control">
+                            <option value="-1">- انتخاب مشخصه کالا -</option>
+                            @foreach($att as $attribute)
+                                <option value="{{$attribute->id}}">-- {{$attribute->title}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" wire:model.lazy="attribute.value" placeholder=" مقدار مشخصه کالا "
+                               class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <div class="notificationGroup">
+                            <input id="option4" type="checkbox" wire:model.lazy="attribute.status" name="status"
+                                   class="form-control">
+                            <label for="option4">نمایش در مقدار مشخصات کالا:</label>
+                        </div>
+                    </div>
+
+                    <button class="btn btn-brand">افزودن مقدار مشخصه کالا</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
