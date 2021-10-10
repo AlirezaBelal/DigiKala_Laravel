affc62a6b24ef166c3349b1c985bf7edd322d143
Alireza Belal <belal.alireza@yahoo.com>
2021-10-10 10:18:22 +0330

    Correct misspellings

--- resources/views/livewire/admin/dashbord/breadcrumb.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="breadcrumb">
-    <ul>
-        <li><a href="/admin">پیشخوان</a></li>
-        <li><a href="{{request()->url()}}" class="is-active">@yield('title')</a></li>
-    </ul>
-</div>
