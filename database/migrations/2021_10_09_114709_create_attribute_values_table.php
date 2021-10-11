5a87dd7961880fadf39e21a43ccd452a44435656
Alireza Belal <belal.alireza@yahoo.com>
2021-10-11 13:38:13 +0330

    Database of attribute values

---
+++ database/migrations/2021_10_09_114709_create_attribute_values_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttributeValuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attribute_values', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('product_id')->nullable();
+            $table->integer('attribute_id')->nullable();
+            $table->string('status');
+            $table->string('value');
+
+            $table->softDeletes();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attribute_values');
+    }
+}
