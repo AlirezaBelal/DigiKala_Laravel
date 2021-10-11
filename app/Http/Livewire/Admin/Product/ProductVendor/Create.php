02ff6e51cd851993ba52cbc14dc382261df3a976
Alireza Belal <belal.alireza@yahoo.com>
2021-10-11 10:11:07 +0330

    Add page to create vomiting variety

---
+++ app/Http/Livewire/Admin/Product/ProductVendor/Create.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Product\ProductVendor;
+
+use App\Models\Log;
+use App\Models\Product;
+use App\Models\ProductSeller;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+
+class Create extends Component
+{
+    use WithFileUploads;
+
+    public ProductSeller $productSeller;
+
+    protected $rules = [
+        'productSeller.product_id' => 'nullable',
+        'productSeller.vendor_id' => 'required',
+        'productSeller.time' => 'required',
+        'productSeller.warranty_id' => 'required',
+        'productSeller.price' => 'required',
+        'productSeller.discount_price' => 'required',
+        'productSeller.color_id' => 'required',
+        'productSeller.product_count' => 'required',
+        'productSeller.limit_order' => 'required',
+        'productSeller.status' => 'nullable',
+    ];
+
+
+    public function mount()
+    {
+        $this->productSeller = new ProductSeller();
+    }
+
+
+    public function updated($title)
+    {
+        $this->validateOnly($title);
+    }
+
+
+    public function categoryForm()
+    {
+
+        $this->validate();
+        $this->productSeller->save();
+
+        Log::create([
+            'user_id' => auth()->user()->id,
+            'url' => 'افزودن تنوع قیمت محصول' . '-' . $this->productSeller->title,
+            'actionType' => 'ایجاد'
+        ]);
+
+        alert()->success(' با موفقیت ایجاد شد.', 'تنوع قیمت محصول مورد نظر با موفقیت ایجاد شد.');
+        return redirect(route('productVendor.index'));
+    }
+
+
+    public function render()
+    {
+        return view('livewire.admin.product.product-vendor.create');
+    }
+}
