4448b069faf8ffc99aeebbd8af3e90a7d1effadb
Alireza Belal <belal.alireza@yahoo.com>
2021-10-11 13:45:17 +0330

    Product warranty database

---
+++ database/migrations/2021_10_08_222001_create_warranties_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWarrantiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('warranties', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name')->nullable();
+            $table->string('status')->default(1);
+
+            $table->softDeletes();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('warranties');
+    }
+}
