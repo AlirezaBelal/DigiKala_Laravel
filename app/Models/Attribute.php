b90e37d1aed3831720d5f99e4ee1e24d878d8295
Alireza Belal <belal.alireza@yahoo.com>
2021-10-11 13:40:20 +0330

    Product Features Database

---
+++ app/Models/Attribute.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Attribute extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'childCategory',
+        'parent',
+        'position',
+        'status'];
+}
---
+++ database/migrations/2021_10_09_102021_create_attributes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttributesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attributes', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->integer('childCategory')->nullable();
+            $table->integer('parent')->default(0);
+            $table->string('status');
+            $table->string('position');
+
+            $table->softDeletes();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attributes');
+    }
+}
