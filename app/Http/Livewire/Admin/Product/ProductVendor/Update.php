9b64a953587972c2545be7d07c8e46f0ae93c87b
Alireza Belal <belal.alireza@yahoo.com>
2021-10-11 10:45:28 +0330

    Add price variation update page

---
+++ app/Http/Livewire/Admin/Product/ProductVendor/Update.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Product\ProductVendor;
+
+use App\Models\Category;
+use App\Models\Log;
+use App\Models\Product;
+use App\Models\ProductSeller;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+
+class Update extends Component
+{
+    use WithFileUploads;
+
+    public ProductSeller $productSeller;
+
+    protected $rules = [
+        'productSeller.product_id' => 'nullable',
+        'productSeller.vendor_id' => 'required',
+        'productSeller.time' => 'required',
+        'productSeller.warranty_id' => 'required',
+        'productSeller.price' => 'required',
+        'productSeller.discount_price' => 'required',
+        'productSeller.color_id' => 'required',
+        'productSeller.product_count' => 'required',
+        'productSeller.limit_order' => 'required',
+        'productSeller.status' => 'nullable',
+    ];
+
+
+    public function categoryForm()
+    {
+        $this->validate();
+        $this->productSeller->update($this->validate());
+
+        Log::create([
+            'user_id' => auth()->user()->id,
+            'url' => 'آپدیت تنوع محصول' . '-' . $this->productSeller->product_id,
+            'actionType' => 'آپدیت'
+        ]);
+        alert()->success(' با موفقیت آپدیت شد.', 'تنوع محصول مورد نظر با موفقیت آپدیت شد.');
+        return redirect(route('productVendor.index'));
+
+    }
+
+
+    public function render()
+    {
+        if ($this->productSeller->status == 1) {
+            $this->productSeller->status = true;
+        } else {
+            $this->productSeller->status = false;
+        }
+        $productSeller = $this->productSeller;
+
+        return view('livewire.admin.product.product-vendor.update', compact('productSeller'));
+    }
+}
---
+++ resources/views/livewire/admin/product/product-vendor/update.blade.php
@@ -0,0 +1,124 @@
+@section('title','آپدیت تنوع محصول')
+
+<div>
+    <div class="main-content padding-0">
+        <p class="box__title">
+            ویرایش محصول -
+            @foreach(\App\Models\Product::where('id',$productSeller->product_id)->get() as $productSellerProduct)
+                {{$productSellerProduct->title}}
+            @endforeach
+        </p>
+
+        <div class="row no-gutters bg-white">
+            <div class="col-12">
+                <form wire:submit.prevent="categoryForm"
+                      enctype="multipart/form-data" role="form"
+                      class="padding-10 categoryForm">
+
+                    @include('errors.error')
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <select wire:model.lazy="productSeller.product_id" name="product_id" id=""
+                                        class="form-control">
+                                    <option value="-1">-محصول-</option>
+                                    @foreach(\App\Models\Product::all() as $product)
+                                        <option value="{{$product->id}}">{{$product->title}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <select wire:model.lazy="productSeller.vendor_id" name="vendor_id" id=""
+                                        class="form-control">
+                                    <option value="-1">-فروشنده-</option>
+                                    @foreach(\App\Models\User::all() as $user)
+                                        <option value="{{$user->id}}">{{$user->name}} - {{$user->email}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <select wire:model.lazy="productSeller.warranty_id" name="warranty_id" id=""
+                                        class="form-control">
+                                    <option value="-1">-گارانتی-</option>
+                                    @foreach(\App\Models\Warranty::all() as $warranty)
+                                        <option value="{{$warranty->id}}">{{$warranty->name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <select wire:model.lazy="productSeller.color_id" name="color_id" id=""
+                                        class="form-control">
+                                    <option value="-1">-رنگ-</option>
+                                    @foreach(\App\Models\Color::all() as $color)
+                                        <option value="{{$color->id}}"
+                                                style="background-color: {{$color->value}}">{{$color->name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-3">
+                            <input type="text" wire:model.lazy="productSeller.product_count"
+                                   placeholder="تعداد موجودی "
+                                   class="form-control">
+                        </div>
+
+                        <div class="col-md-3">
+                            <input type="text" wire:model.lazy="productSeller.limit_order"
+                                   placeholder="تعداد سفارش (صفر نامحدود) "
+                                   class="form-control">
+                        </div>
+
+                        <div class="col-md-3 ">
+                            <input type="text" wire:model.lazy="productSeller.price" placeholder="قیمت  محصول "
+                                   class="form-control">
+                        </div>
+
+                        <div class="col-md-3 ">
+                            <input type="text" wire:model.lazy="productSeller.discount_price"
+                                   placeholder="قیمت تخفیف خورده "
+                                   class="form-control">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6 ">
+                            <div class="form-group">
+                                <input type="text" wire:model.lazy="productSeller.time"
+                                       placeholder="زمان ارسال محصول به روز "
+                                       class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <div class="notificationGroup">
+                                    <input id="option10" type="checkbox" wire:model.lazy="productSeller.status"
+                                           name="status"
+                                           class="form-control">
+                                    <label for="option10">وضعیت محصول:</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <button class="btn btn-brand">آپدیت تنوع محصول</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
