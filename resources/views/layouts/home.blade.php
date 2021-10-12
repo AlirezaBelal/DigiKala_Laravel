045a83051040dc12cb54d699c36f31c11646786d
Alireza Belal <belal.alireza@yahoo.com>
2021-10-12 23:58:52 +0330

    add home layouts

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    @include('livewire.home.home.head')
+</head>
+<body class=" t-index" style="">
+
+
+@include('sweet::alert')
+{{--@include('livewire.home.home.header')--}}
+{{--    {{$slot}}--}}
+
+{{--@include('livewire.home.home.footer')--}}
+</body>
+
+{{--@include('livewire.home.home.jscript')--}}
+
+</html>
