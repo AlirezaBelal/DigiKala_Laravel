cb2467f9cea58e1e0c8830110301793e012dec81
Alireza Belal <belal.alireza@yahoo.com>
2021-10-15 14:47:58 +0330

    add class setting site

---
+++ app/Http/Livewire/Home/Home/Setting/Footer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Home\Home\Setting;
+
+use Livewire\Component;
+
+class Footer extends Component
+{
+    public function render()
+    {
+        return view('livewire.home.home.setting.footer');
+    }
+}
---
+++ app/Http/Livewire/Home/Home/Setting/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Home\Home\Setting;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.home.home.setting.index');
+    }
+}
