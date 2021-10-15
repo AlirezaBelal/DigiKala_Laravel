3f89cde46a509bc68d78d98198d5d78cb3056c34
Alireza Belal <belal.alireza@yahoo.com>
2021-10-15 14:24:38 +0330

    Add site header tasks

---
+++ app/Http/Livewire/Admin/Site/Header/Index.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Site\Header;
+
+use App\Models\Category;
+use App\Models\Log;
+use App\Models\SiteHeader;
+use App\Models\SubCategory;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+use Livewire\WithPagination;
+
+class Index extends Component
+{
+    use WithFileUploads;
+    use WithPagination;
+
+    public SiteHeader $header;
+    public $img;
+    public $search;
+    public $readyToLoad = false;
+
+
+    protected $queryString = ['search'];
+    protected $paginationTheme = 'bootstrap';
+
+
+    protected $rules = [
+        'header.title' => 'required|min:3',
+        'header.status' => 'nullable',
+        'header.link' => 'required',
+        'header.icon' => 'nullable',
+    ];
+
+
+    public function mount()
+    {
+        $this->header = new SiteHeader();
+    }
+
+
+    /**
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function updated($title)
+    {
+        $this->validateOnly($title);
+    }
+
+
+    public function loadCategory()
+    {
+        $this->readyToLoad = true;
+    }
+
+
+    public function categoryForm()
+    {
+
+        $this->validate();
+
+        $header = SiteHeader::query()->create([
+            'title' => $this->header->title,
+            'link' => $this->header->link,
+            'icon' => $this->header->icon,
+            'status' => $this->header->status ? true : false,
+        ]);
+
+        if ($this->img) {
+            $header->update([
+                'img' => $this->uploadImage()
+            ]);
+        }
+
+        $this->header->title = "";
+        $this->header->link = "";
+        $this->header->icon = "";
+        $this->header->status = false;
+        $this->img = null;
+
+        Log::create([
+            'user_id' => auth()->user()->id,
+            'url' => 'افزودن منو هدر' . '-' . $this->header->title,
+            'actionType' => 'ایجاد'
+        ]);
+        $this->emit('toast', 'success', ' منو هدر با موفقیت ایجاد شد.');
+    }
+
+
+    public function uploadImage()
+    {
+        $year = now()->year;
+        $month = now()->month;
+        $directory = "site/$year/$month";
+        $name = $this->img->getClientOriginalName();
+        $this->img->storeAs($directory, $name);
+        return "$directory/$name";
+    }
+
+
+    public function updateCategoryDisable($id)
+    {
+        $header = SiteHeader::find($id);
+        $header->update([
+            'status' => 0
+        ]);
+        Log::create([
+            'user_id' => auth()->user()->id,
+            'url' => 'غیرفعال کردن وضعیت منو هدر' . '-' . $header->title,
+            'actionType' => 'غیرفعال'
+        ]);
+        $this->emit('toast', 'success', 'وضعیت منو هدر با موفقیت غیرفعال شد.');
+    }
+
+
+    public function updateCategoryEnable($id)
+    {
+        $header = SiteHeader::find($id);
+        $header->update([
+            'status' => 1
+        ]);
+        Log::create([
+            'user_id' => auth()->user()->id,
+            'url' => 'فعال کردن وضعیت منو هدر' . '-' . $header->title,
+            'actionType' => 'فعال'
+        ]);
+        $this->emit('toast', 'success', 'وضعیت منو هدر با موفقیت فعال شد.');
+    }
+
+
+    public function deleteCategory($id)
+    {
+        $header = SiteHeader::find($id);
+        $header->delete();
+        Log::create([
+            'user_id' => auth()->user()->id,
+            'url' => 'حذف کردن منو هدر' . '-' . $header->title,
+            'actionType' => 'حذف'
+        ]);
+        $this->emit('toast', 'success', ' منو هدر با موفقیت حذف شد.');
+    }
+
+
+    public function render()
+    {
+
+        $headers = $this->readyToLoad ? SiteHeader::where('title', 'LIKE', "%{$this->search}%")
+            ->orWhere('id', $this->search)
+            ->latest()->paginate(10) : [];
+
+        return view('livewire.admin.site.header.index', compact('headers'));
+    }
+}
---
+++ app/Http/Livewire/Admin/Site/Header/Update.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Site\Header;
+
+use App\Models\header;
+use App\Models\Log;
+use App\Models\SiteHeader;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+
+class Update extends Component
+{
+    use WithFileUploads;
+
+    public SiteHeader $header;
+    public $img;
+    public $status = null;
+
+
+    protected $rules = [
+        'header.title' => 'required|min:3',
+        'header.status' => 'required',
+        'header.link' => 'required',
+        'header.icon' => 'nullable',
+    ];
+
+
+    public function headerForm()
+    {
+        $this->validate();
+        if ($this->img) {
+            $this->header->img = $this->uploadImage();
+        }
+        $this->header->update($this->validate());
+        if (!$this->header->status) {
+            $this->header->update([
+                'status' => 0
+            ]);
+        }
+
+        Log::create([
+            'user_id' => auth()->user()->id,
+            'url' => 'آپدیت منو' . '-' . $this->header->title,
+            'actionType' => 'آپدیت'
+        ]);
+        alert()->success('منو با موفقیت ایجاد شد.', 'منو آپدیت شد.');
+        return redirect(route('header.index'));
+    }
+
+    public function uploadImage()
+    {
+        $year = now()->year;
+        $month = now()->month;
+        $directory = "site/$year/$month";
+        $name = $this->img->getClientOriginalName();
+        $this->img->storeAs($directory, $name);
+        return "$directory/$name";
+    }
+
+    public function render()
+    {
+        if ($this->header->status == 1) {
+            $this->header->status = true;
+        } else {
+            $this->header->status = false;
+        }
+
+        $header = $this->header;
+        return view('livewire.admin.site.header.update', compact('header'));
+    }
+}
