b81cd1538e9ceb6780ae5a321c7d2ef4d6fcb2c1
Alireza Belal <belal.alireza@yahoo.com>
2021-10-15 14:26:58 +0330

    Add site social networking tasks

---
+++ app/Http/Livewire/Admin/Social/Index.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Social;
+
+use App\Models\Log;
+use App\Models\Social;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class Index extends Component
+{
+    use WithPagination;
+
+    public Social $social;
+    public $search;
+    public $readyToLoad = false;
+
+    protected $queryString = ['search'];
+    protected $paginationTheme = 'bootstrap';
+
+    protected $rules = [
+        'social.img' => 'nullable',
+        'social.icon' => 'nullable',
+        'social.title' => 'required',
+        'social.link' => 'required',
+    ];
+
+
+    public function mount()
+    {
+        $this->social = new Social();
+    }
+
+
+    /**
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function updated($title)
+    {
+        $this->validateOnly($title);
+    }
+
+
+    public function loadCategory()
+    {
+        $this->readyToLoad = true;
+    }
+
+
+    public function categoryForm()
+    {
+        $this->validate();
+
+        Social::query()->create([
+            'img' => $this->social->img,
+            'icon' => $this->social->icon,
+            'title' => $this->social->title,
+            'link' => $this->social->link,
+        ]);
+
+
+        $this->social->img = "";
+        $this->social->icon = "";
+        $this->social->title = "";
+        $this->social->link = "";
+
+        Log::create([
+            'user_id' => auth()->user()->id,
+            'url' => 'افزودن شبکه اجتماعی' . '-' . $this->social->title,
+            'actionType' => 'ایجاد'
+        ]);
+        $this->emit('toast', 'success', ' شبکه اجتماعی با موفقیت ایجاد شد.');
+
+    }
+
+
+    public function deleteCategory($id)
+    {
+        $page = Social::find($id);
+        $page->delete();
+        Log::create([
+            'user_id' => auth()->user()->id,
+            'url' => 'حذف کردن شبکه اجتماعی' . '-' . $this->social->title,
+            'actionType' => 'حذف'
+        ]);
+        $this->emit('toast', 'success', ' شبکه اجتماعی با موفقیت حذف شد.');
+
+    }
+
+
+    public function render()
+    {
+
+        $socials = $this->readyToLoad ? Social::where('title', 'LIKE', "%{$this->search}%")
+            ->orWhere('link', 'LIKE', "%{$this->search}%")
+            ->orWhere('icon', 'LIKE', "%{$this->search}%")
+            ->latest()->paginate(10) : [];
+        return view('livewire.admin.social.index', compact('socials'));
+    }
+}
---
+++ resources/views/livewire/admin/social/index.blade.php
@@ -0,0 +1,108 @@
+@section('title','شبکه های اجتماعی')
+
+<div>
+    <div class="main-content" wire:init="loadCategory">
+        <div class="tab__box">
+            <div class="tab__items">
+                <a class="tab__item is-active" href="{{route('social.index')}}">
+                    شبکه های اجتماعی
+                </a>
+                |
+                <a class="tab__item">جستجو: </a>
+
+                <a class="t-header-search">
+                    <form action="" onclick="event.preventDefault();">
+                        <input wire:model.debounce.1000="search"
+                               type="text" class="text" placeholder="جستجوی شبکه اجتماعی ...">
+                    </form>
+                </a>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-8 margin-left-10 margin-bottom-15 border-radius-3">
+                <div class="table__box">
+                    <table class="table">
+
+                        <thead role="rowgroup">
+                        <tr role="row" class="title-row">
+                            <th>ردیف</th>
+                            <th>آیکون شبکه اجتماعی</th>
+                            <th>عنوان شبکه اجتماعی</th>
+                            <th>لینک شبکه اجتماعی</th>
+                            <th>عملیات</th>
+                        </tr>
+                        </thead>
+
+                        @if($readyToLoad)
+                            @php($count = 1)
+                            <tbody>
+                            @foreach($socials as $social)
+                                <tr role="row">
+                                    <td>
+                                        {{$count++}}
+                                    </td>
+
+                                    <td>
+                                        <a href="{{$social->link}}"
+                                           class="c-footer__social-link c-footer__social-link--{{$social->icon}}"
+                                           target="_blank"></a>
+                                    </td>
+
+                                    <td>
+                                        {{$social->title}}
+                                    </td>
+
+                                    <td>
+                                        <a target="_blank" href="{{$social->link}}">
+                                            {{$social->link}}
+                                        </a>
+                                    </td>
+
+                                    <td>
+                                        <a wire:click="deleteCategory({{$social->id}})" type="submit"
+                                           class="item-delete mlg-15"
+                                           title="حذف">
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                            {{$socials->render()}}
+                        @else
+                            <div class="alert-warning alert">
+                                در حال خواندن اطلاعات از دیتابیس ...
+                            </div>
+                        @endif
+                    </table>
+                </div>
+            </div>
+
+            <div class="col-4 bg-white">
+                <p class="box__title">ایجاد شبکه اجتماعی جدید</p>
+                <form wire:submit.prevent="categoryForm"
+                      enctype="multipart/form-data" role="form"
+                      class="padding-10 categoryForm">
+
+                    @include('errors.error')
+
+                    <div class="form-group">
+                        <input type="text" wire:model.lazy="social.icon" placeholder="آیکون شبکه اجتماعی "
+                               class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" wire:model.lazy="social.title" placeholder="عنوان شبکه اجتماعی "
+                               class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" wire:model.lazy="social.link" placeholder="لینک شبکه اجتماعی "
+                               class="form-control">
+                    </div>
+
+                    <button class="btn btn-brand">افزودن شبکه اجتماعی</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
