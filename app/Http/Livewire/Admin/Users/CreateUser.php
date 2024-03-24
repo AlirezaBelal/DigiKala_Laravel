3f2ddbbcf5843b20aa142c50c50ee06fa996bcec
AlirezaBelal <Belal.alireza@gmail.com>
2024-03-24 15:52:30 +0330

    Add CreateUser Livewire Component for Admin Panel

---
+++ app/Http/Livewire/Admin/Users/CreateUser.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Users;
+
+use Livewire\Component;
+
+class CreateUser extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.users.create-user');
+    }
+}
