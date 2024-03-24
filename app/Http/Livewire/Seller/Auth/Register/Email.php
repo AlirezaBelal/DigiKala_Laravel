9cc87c30595698cc18b586d138cf0bf2f7268b5d
AlirezaBelal <Belal.alireza@gmail.com>
2024-03-24 15:59:17 +0330

    Implement Email Verification for Seller Registration

---
+++ app/Http/Livewire/Seller/Auth/Register/Email.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Livewire\Seller\Auth\Register;
+
+use App\Mail\SellerRegister;
+use App\Models\Seller;
+use Illuminate\Support\Facades\Mail;
+use Livewire\Component;
+
+class Email extends Component
+{
+    public Seller $seller;
+
+    public $code;
+
+
+    public function registerEmailVerify()
+    {
+
+        $usr = \App\Models\Email::where('user_email', $this->seller->email)->where('code', $this->code)->first();
+
+        if ($usr) {
+
+            return $this->redirect(route('seller.register.detail', $usr->user_id));
+        } else {
+
+            $this->emit('toast', 'error', ' کد وارد شده صحیح نمی باشد.');
+        }
+
+
+    }
+
+    public function render()
+    {
+        $seller = $this->seller;
+        return view('livewire.seller.auth.register.email'
+            , compact('seller'))->layout('layouts.seller');
+    }
+}
