25be16d12f3c8c97437dbb7d7b732e726bc5d73b
AlirezaBelal <belal.alireza@yahoo.com>
2024-03-24 17:26:40 +0330

    Define relationships and fillable attributes in RoleUser model

---
+++ app/Models/RoleUser.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RoleUser extends Model
+{
+    use HasFactory;
+
+    protected $table = 'role_user';
+    protected $fillable = ['role_id', 'user_id'];
+    public $timestamps = false;
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'role_user', 'role_id', 'id');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
