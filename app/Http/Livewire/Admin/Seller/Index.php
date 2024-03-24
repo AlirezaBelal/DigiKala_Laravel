f14734382012db504550db6d14658fb084dd6901
AlirezaBelal <Belal.alireza@gmail.com>
2024-03-24 15:38:21 +0330

    Enhance Seller management with approval process and notifications

--- app/Http/Livewire/Admin/Seller/Index.php
+++ app/Http/Livewire/Admin/Seller/Index.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Livewire\Admin\Seller;
 
+use App\Mail\OrderSubmit;
+use App\Models\Notification;
 use App\Models\Seller;
 use App\Models\Log;
 use App\Models\Product;
+use Illuminate\Support\Facades\Mail;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 use Livewire\WithPagination;
@@ -58,6 +61,34 @@ class Index extends Component
             $this->emit('toast', 'success', ' امکان حذف وجود ندارد زیرا فروشنده، شامل محصول است!');
         }
 
+    }
+
+    public function acceptSeller($id)
+    {
+        $seller = seller::find($id);
+        $seller->update([
+            'learning_status' =>1
+        ]);
+        $type = 'تایید فروشندگی شما';
+        Notification::create([
+            'user_id' => $seller->user_id,
+            'type' =>$type,
+            'sms' =>0,
+            'email' =>1,
+            'system' =>1,
+            'text' =>$type,
+        ]);
+        $email = \App\Models\Email::create([
+            'user_id' =>$seller->id,
+            'user_email' =>$seller->email,
+            'user_mobile' =>$seller->mobile,
+            'title' =>'تایید فروشندگی شما',
+            'text' =>'تایید فروشندگی شما',
+            'code' =>'تایید فروشندگی شما',
+        ]);
+
+        Mail::to($seller->email)->send(new OrderSubmit($email));
+        $this->emit('toast', 'success', ' فروشنده با موفقیت تایید شد.');
     }
 
 
