d58d0c88866cfa6fd3bea773851036bbedb1ccb5
AlirezaBelal <Belal.alireza@gmail.com>
2024-03-24 14:49:43 +0330

    Add seller dashboard, profile, and product management routes
    - Introduce a seller dashboard route for an overview of seller activities and metrics.
    - Add a profile management route for sellers to update their information.
    - Implement a product finding route to allow sellers to search for their products easily.
    - Create routes for product listing and management, enabling sellers to view and modify their product details.
    - Establish an orders management route for sellers to track and manage orders.
    - Add a product creation route to facilitate the addition of new products by sellers.
    
    These routes collectively enhance the seller's ability to manage their sales, products, and profile directly from the seller dashboard.

---
+++ routes/seller.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//======================================>seller/dashboard
+Route::get('/', App\Http\Livewire\Seller\Dashboard\Index::class)
+    ->name('seller.dashboard.index');
+//======================================>seller/profile
+Route::get('/profile', App\Http\Livewire\Seller\Dashboard\Profile::class)
+    ->name('seller.dashboard.profile');
+
+//======================================>seller/find/product
+Route::get('/content/find/product', App\Http\Livewire\Seller\Product\Find::class)
+    ->name('seller.product.find');
+//======================================>seller/product
+Route::get('/product', App\Http\Livewire\Seller\Product\Product::class)
+    ->name('seller.product.product');
+
+//======================================>orders
+Route::get('/orders', App\Http\Livewire\Seller\Orders\Order::class)
+    ->name('seller.order.order');
+
+//======================================>seller/create/product
+Route::get('/content/create/product', App\Http\Livewire\Seller\Product\Create::class)
+    ->name('seller.product.create');
+
+
