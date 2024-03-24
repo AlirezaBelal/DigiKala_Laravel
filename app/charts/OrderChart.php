2d7a022f75a4241d1b9a611b417b173722010dd0
AlirezaBelal <Belal.alireza@gmail.com>
2024-03-24 14:53:51 +0330

    Create custom OrderChart class for orders visualization
    - Implement OrderChart class extending Chartjs\Chart for dynamic orders data visualization.
    - Customize chart appearance including aspect ratio, legend configuration, and axes scales.
    - Define initial dataset with configurable data and labels for flexibility.
    - Set detailed styling for the dataset to enhance visual appeal, including background color, border color, and hover effects.
    - Opt for a constructor-based configuration approach to allow passing datasets and labels dynamically, enhancing reusability across different views or data sets.
    
    This class provides a foundational setup for visualizing orders data, aiming to offer insightful and interactive data representation for users.

---
+++ app/charts/OrderChart.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Charts;
+
+use Chartisan\PHP\Chartisan;
+use ConsoleTVs\Charts\Classes\Chartjs\Chart;
+use Illuminate\Http\Request;
+
+class OrderChart extends Chart
+{
+
+    /**
+     * Handles the HTTP request for the given chart.
+     * It must always return an instance of Chartisan
+     * and never a string or an array.
+     * @param array $datasets
+     * @param array $labels
+     */
+
+    public function __construct($datasets = [[]], $labels = [])
+    {
+        parent::__construct();
+
+        $this->loader(false);
+
+        $this->options([
+            'maintainAspectRatio' => false,
+            'legend' => [
+                'display' => true,
+            ],
+            'scales' => [
+                'yAxes' => [
+                    [
+                        'ticks' => [
+                            'maxTicksLimit' => 6,
+                            'beginAtZero' => false,
+                        ],
+                    ],
+                ],
+                'xAxes' => [
+                    [
+                        'display' => true,
+                    ],
+                ],
+            ],
+        ]);
+
+        $this->labels($labels);
+
+        $this->dataset("سفارشات", "line", $datasets[0])->options([
+            'backgroundColor' => 'rgb(127,156,245, 0.4)',
+            'borderColor' => '#7F9CF5',
+            'pointBackgroundColor' => 'rgb(255, 255, 255, 0)',
+            'pointBorderColor' => 'rgb(255, 255, 255, 0)',
+            'pointHoverBackgroundColor' => '#7F9CF5',
+            'pointHoverBorderColor' => '#7F9CF5',
+            'borderWidth' => 1,
+            'pointRadius' => 1,
+        ]);
+    }
+
+
+//    /**
+//     * Handles the HTTP request for the given chart.
+//     * It must always return an instance of Chartisan
+//     * and never a string or an array.
+//     * @param Request $request
+//     * @return Chartisan
+//     */
+//    public function handler(Request $request): Chartisan
+//    {
+//        return Chartisan::build()
+//            ->labels(['First', 'Second', 'Third'])
+//            ->dataset('Sample', [1, 2, 3])
+//            ->dataset('Sample 2', [3, 2, 1]);
+//    }
+}
