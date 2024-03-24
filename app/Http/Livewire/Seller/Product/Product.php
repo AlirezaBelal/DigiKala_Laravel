3669c0456e15964021dcaef1f86ea2711a4e810e
AlirezaBelal <Belal.alireza@gmail.com>
2024-03-24 16:10:38 +0330

    Add Seller Product Livewire Component

---
+++ app/Http/Livewire/Seller/Product/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Livewire\Seller\Product;
+
+use Livewire\Component;
+
+class Product extends Component
+{
+    public $search;
+    protected $queryString = ['search'];
+
+    public function render()
+    {
+
+        $products = \App\Models\Product::where('vendor_id', auth()->user()->id)->
+        where('status_product', 1)->get();
+        return view('livewire.seller.product.product', compact('products'))
+            ->layout('layouts.seller_dashboard');
+    }
+}
