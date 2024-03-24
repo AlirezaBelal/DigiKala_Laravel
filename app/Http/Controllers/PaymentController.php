af13f8eb6928c3de468ac4e74c82b1e8a6e6ce8b
AlirezaBelal <Belal.alireza@gmail.com>
2024-03-24 15:26:59 +0330

    Refactor Zarinpal payment process in PaymentController
    - Remove commented-out alert messages to streamline user experience during payment initiation.
    - Update the payment method to use the Zarinpal class for processing payment requests, including setting up the merchant ID, amount, description, email, mobile, and callback URL dynamically.
    - Enhance the callback method for payment verification with comprehensive checks on the transaction status and updates on related models such as BankPayment, Order, and Payment records based on the callback response.
    - Simplify the methods for checking cURL and SOAP extensions availability, error message handling, determining the Zarinpal service node, and redirecting users post-payment.
    - Ensure consistent error handling and user feedback for both success and failure scenarios in payment transactions.
    
    These changes aim to enhance the reliability and user experience of the payment process through cleaner code and improved error handling.

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -12,35 +12,34 @@ class PaymentController extends Controller
     public function payment()
     {
 //        require_once("app/zarinpal_function.php");
-        $bank = \App\Models\BankPayment::where('user_id',auth()->user()->id)
+        $bank = \App\Models\BankPayment::where('user_id', auth()->user()->id)
             ->get()->last();
         if ($bank->status == 1) {
-            alert()->message('شما قبلا این سفارش را خریداری کرده اید.');
+//            alert()->message('شما قبلا این سفارش را خریداری کرده اید.');
             return back();
         }
-        $description ="خرید از سایت دیجی کالا - تست";
-        $callbackUrl ='http://127.0.0.1:8000/payment/zarrinpal/callback';
+        $description = "خرید از سایت دیجی کالا - تست";
+        $callbackUrl = 'http://127.0.0.1:8000/payment/zarrinpal/callback';
 //        $client = new SoapClient("https://www.zarinpal.com/pg/services/WebGate/wsdl", ['encoding' => 'UTF-8']);
-        $MerchantID 	= "69a09ffa-8521-11ea-8c16-000c295eb8fc";
-        $Amount 		= $bank->price;
-        $Description 	= $description;
-        $Email 			= auth()->user()->email;
-        $Mobile 		= auth()->user()->mobile;
-        $CallbackURL 	= $callbackUrl;
-        $ZarinGate 		= false;
-        $SandBox 		= false;
-        $zp 	= new zarinpal();
+        $MerchantID = "69a09ffa-8521-11ea-8c16-000c295eb8fc";
+        $Amount = $bank->price;
+        $Description = $description;
+        $Email = auth()->user()->email;
+        $Mobile = auth()->user()->mobile;
+        $CallbackURL = $callbackUrl;
+        $ZarinGate = false;
+        $SandBox = false;
+        $zp = new zarinpal();
         $result = $zp->request($MerchantID, $Amount, $Description, $Email, $Mobile, $CallbackURL, $SandBox, $ZarinGate);
 
-        if (isset($result["Status"]) && $result["Status"] == 100)
-        {
+        if (isset($result["Status"]) && $result["Status"] == 100) {
             // Success and redirect to pay
             $zp->redirect($result["StartPay"]);
         } else {
             // error
             echo "خطا در ایجاد تراکنش";
-            echo "<br />کد خطا : ". $result["Status"];
-            echo "<br />تفسیر و علت خطا : ". $result["Message"];
+            echo "<br />کد خطا : " . $result["Status"];
+            echo "<br />تفسیر و علت خطا : " . $result["Message"];
         }
     }
 
@@ -59,40 +58,37 @@ class PaymentController extends Controller
         return (extension_loaded('soap')) ? true : false;
     }
 
-    private function error_message($code, $desc, $cb, $request=false)
+    private function error_message($code, $desc, $cb, $request = false)
     {
-        if (empty($cb) && $request === true)
-        {
+        if (empty($cb) && $request === true) {
             return "لینک بازگشت ( CallbackURL ) نباید خالی باشد";
         }
 
-        if (empty($desc) && $request === true)
-        {
+        if (empty($desc) && $request === true) {
             return "توضیحات تراکنش ( Description ) نباید خالی باشد";
         }
 
 
         $error = array(
-            "-1" 	=> "اطلاعات ارسال شده ناقص است.",
-            "-2" 	=> "IP و يا مرچنت كد پذيرنده صحيح نيست",
-            "-3" 	=> "با توجه به محدوديت هاي شاپرك امكان پرداخت با رقم درخواست شده ميسر نمي باشد",
-            "-4" 	=> "سطح تاييد پذيرنده پايين تر از سطح نقره اي است.",
-            "-11" 	=> "درخواست مورد نظر يافت نشد.",
-            "-12" 	=> "امكان ويرايش درخواست ميسر نمي باشد.",
-            "-21" 	=> "هيچ نوع عمليات مالي براي اين تراكنش يافت نشد",
-            "-22" 	=> "تراكنش نا موفق ميباشد",
-            "-33" 	=> "رقم تراكنش با رقم پرداخت شده مطابقت ندارد",
-            "-34" 	=> "سقف تقسيم تراكنش از لحاظ تعداد يا رقم عبور نموده است",
-            "-40" 	=> "اجازه دسترسي به متد مربوطه وجود ندارد.",
-            "-41" 	=> "اطلاعات ارسال شده مربوط به AdditionalData غيرمعتبر ميباشد.",
-            "-42" 	=> "مدت زمان معتبر طول عمر شناسه پرداخت بايد بين 30 دقيه تا 45 روز مي باشد.",
-            "-54" 	=> "درخواست مورد نظر آرشيو شده است",
-            "100" 	=> "عمليات با موفقيت انجام گرديده است.",
-            "101" 	=> "عمليات پرداخت موفق بوده و قبلا PaymentVerification تراكنش انجام شده است.",
+            "-1" => "اطلاعات ارسال شده ناقص است.",
+            "-2" => "IP و يا مرچنت كد پذيرنده صحيح نيست",
+            "-3" => "با توجه به محدوديت هاي شاپرك امكان پرداخت با رقم درخواست شده ميسر نمي باشد",
+            "-4" => "سطح تاييد پذيرنده پايين تر از سطح نقره اي است.",
+            "-11" => "درخواست مورد نظر يافت نشد.",
+            "-12" => "امكان ويرايش درخواست ميسر نمي باشد.",
+            "-21" => "هيچ نوع عمليات مالي براي اين تراكنش يافت نشد",
+            "-22" => "تراكنش نا موفق ميباشد",
+            "-33" => "رقم تراكنش با رقم پرداخت شده مطابقت ندارد",
+            "-34" => "سقف تقسيم تراكنش از لحاظ تعداد يا رقم عبور نموده است",
+            "-40" => "اجازه دسترسي به متد مربوطه وجود ندارد.",
+            "-41" => "اطلاعات ارسال شده مربوط به AdditionalData غيرمعتبر ميباشد.",
+            "-42" => "مدت زمان معتبر طول عمر شناسه پرداخت بايد بين 30 دقيه تا 45 روز مي باشد.",
+            "-54" => "درخواست مورد نظر آرشيو شده است",
+            "100" => "عمليات با موفقيت انجام گرديده است.",
+            "101" => "عمليات پرداخت موفق بوده و قبلا PaymentVerification تراكنش انجام شده است.",
         );
 
-        if (array_key_exists("{$code}", $error))
-        {
+        if (array_key_exists("{$code}", $error)) {
             return $error["{$code}"];
         } else {
             return "خطای نامشخص هنگام اتصال به درگاه زرین پال";
@@ -101,8 +97,7 @@ class PaymentController extends Controller
 
     private function zarinpal_node()
     {
-        if ($this->curl_check() === true)
-        {
+        if ($this->curl_check() === true) {
             $ir_ch = curl_init("https://www.zarinpal.com/pg/services/WebGate/wsdl");
             curl_setopt($ir_ch, CURLOPT_TIMEOUT, 1);
             curl_setopt($ir_ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -124,13 +119,12 @@ class PaymentController extends Controller
 
             return ($ir_total_time === false || $ir_total_time > $de_total_time) ? "de" : "ir";
         } else {
-            if (function_exists('fsockopen'))
-            {
-                $de_ping 	= $this->zarinpal_ping("de.zarinpal.com", 80, 1);
-                $ir_ping 	= $this->zarinpal_ping("www.zarinpal.com", 80, 1);
+            if (function_exists('fsockopen')) {
+                $de_ping = $this->zarinpal_ping("de.zarinpal.com", 80, 1);
+                $ir_ping = $this->zarinpal_ping("www.zarinpal.com", 80, 1);
 
-                $de_domain 	= "https://de.zarinpal.com/pg/services/WebGate/wsdl";
-                $ir_domain 	= "https://www.zarinpal.com/pg/services/WebGate/wsdl";
+                $de_domain = "https://de.zarinpal.com/pg/services/WebGate/wsdl";
+                $ir_domain = "https://www.zarinpal.com/pg/services/WebGate/wsdl";
 
                 $ir_total_time = (isset($ir_ping) && $ir_ping > 0) ? $ir_ping : false;
                 $de_total_time = (isset($de_ping) && $de_ping > 0) ? $de_ping : false;
@@ -138,7 +132,7 @@ class PaymentController extends Controller
                 return ($ir_total_time === false || $ir_total_time > $de_total_time) ? "de" : "ir";
             } else {
                 $webservice = "https://www.zarinpal.com/pg/services/WebGate/wsd";
-                $headers 	= @get_headers($webservice);
+                $headers = @get_headers($webservice);
 
                 return (strpos($headers[0], '200') === false) ? "de" : "ir";
             }
@@ -147,13 +141,12 @@ class PaymentController extends Controller
 
     private function zarinpal_ping($host, $port, $timeout)
     {
-        $time_b 	= microtime(true);
-        $fsockopen 	= @fsockopen($host, $port, $errno, $errstr, $timeout);
+        $time_b = microtime(true);
+        $fsockopen = @fsockopen($host, $port, $errno, $errstr, $timeout);
 
-        if (!$fsockopen)
-        {
+        if (!$fsockopen) {
             return false;
-        }  else {
+        } else {
             $time_a = microtime(true);
             return round((($time_a - $time_b) * 1000), 0);
         }
@@ -161,54 +154,53 @@ class PaymentController extends Controller
 
     public function redirect($url)
     {
-        @header('Location: '. $url);
+        @header('Location: ' . $url);
         echo "<meta http-equiv='refresh' content='0; url={$url}' />";
         echo "<script>window.location.href = '{$url}';</script>";
         exit;
     }
 
-    public function request($MerchantID, $Amount, $Description="", $Email="", $Mobile="", $CallbackURL, $SandBox=false, $ZarinGate=false)
+    public function request($MerchantID, $Amount, $Description = "", $Email = "", $Mobile = "", $CallbackURL, $SandBox = false, $ZarinGate = false)
     {
         $ZarinGate = ($SandBox == true) ? false : $ZarinGate;
 
-        if ($this->soap_check() === true)
-        {
-            $node 	= ($SandBox == true) ? "sandbox" : $this->zarinpal_node();
-            $upay 	= ($SandBox == true) ? "sandbox" : "www";
+        if ($this->soap_check() === true) {
+            $node = ($SandBox == true) ? "sandbox" : $this->zarinpal_node();
+            $upay = ($SandBox == true) ? "sandbox" : "www";
 
             $client = new SoapClient("https://{$node}.zarinpal.com/pg/services/WebGate/wsdl", ['encoding' => 'UTF-8']);
 
             $result = $client->PaymentRequest([
-                'MerchantID'     => $MerchantID,
-                'Amount'         => $Amount,
-                'Description'    => $Description,
-                'Email'          => $Email,
-                'Mobile'         => $Mobile,
-                'CallbackURL'    => $CallbackURL,
+                'MerchantID' => $MerchantID,
+                'Amount' => $Amount,
+                'Description' => $Description,
+                'Email' => $Email,
+                'Mobile' => $Mobile,
+                'CallbackURL' => $CallbackURL,
             ]);
 
-            $Status 		= (isset($result->Status) 		&& $result->Status != "") 		? $result->Status : 0;
-            $Authority 		= (isset($result->Authority) 	&& $result->Authority != "") 	? $result->Authority : "";
-            $StartPay 		= (isset($result->Authority) 	&& $result->Authority != "") 	? "https://{$upay}.zarinpal.com/pg/StartPay/". $Authority : "";
-            $StartPayUrl 	= (isset($ZarinGate) 			&& $ZarinGate == true) 			? "{$StartPay}/ZarinGate" : $StartPay;
+            $Status = (isset($result->Status) && $result->Status != "") ? $result->Status : 0;
+            $Authority = (isset($result->Authority) && $result->Authority != "") ? $result->Authority : "";
+            $StartPay = (isset($result->Authority) && $result->Authority != "") ? "https://{$upay}.zarinpal.com/pg/StartPay/" . $Authority : "";
+            $StartPayUrl = (isset($ZarinGate) && $ZarinGate == true) ? "{$StartPay}/ZarinGate" : $StartPay;
 
             return array(
-                "Node" 		=> "{$node}",
-                "Method" 	=> "SOAP",
-                "Status" 	=> $Status,
-                "Message" 	=> $this->error_message($Status, $Description, $CallbackURL, true),
-                "StartPay" 	=> $StartPayUrl,
+                "Node" => "{$node}",
+                "Method" => "SOAP",
+                "Status" => $Status,
+                "Message" => $this->error_message($Status, $Description, $CallbackURL, true),
+                "StartPay" => $StartPayUrl,
                 "Authority" => $Authority
             );
         } else {
-            $node 	= ($SandBox == true) ? "sandbox" : "ir";
-            $upay 	= ($SandBox == true) ? "sandbox" : "www";
+            $node = ($SandBox == true) ? "sandbox" : "ir";
+            $upay = ($SandBox == true) ? "sandbox" : "www";
 
             $data = array(
-                'MerchantID'     => $MerchantID,
-                'Amount'         => $Amount,
-                'Description'    => $Description,
-                'CallbackURL'    => $CallbackURL,
+                'MerchantID' => $MerchantID,
+                'Amount' => $Amount,
+                'Description' => $Description,
+                'CallbackURL' => $CallbackURL,
             );
 
             $jsonData = json_encode($data);
@@ -221,71 +213,69 @@ class PaymentController extends Controller
             curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: ' . strlen($jsonData)));
 
             $result = curl_exec($ch);
-            $err 	= curl_error($ch);
+            $err = curl_error($ch);
             curl_close($ch);
 
             $result = json_decode($result, true);
 
-            if ($err)
-            {
-                $Status 		= 0;
-                $Message 		= "cURL Error #:" . $err;
-                $Authority 		= "";
-                $StartPay 		= "";
-                $StartPayUrl 	= "";
+            if ($err) {
+                $Status = 0;
+                $Message = "cURL Error #:" . $err;
+                $Authority = "";
+                $StartPay = "";
+                $StartPayUrl = "";
             } else {
-                $Status 		= (isset($result["Status"]) 	&& $result["Status"] != "") 	? $result["Status"] : 0;
-                $Message 		= $this->error_message($Status, $Description, $CallbackURL, true);
-                $Authority 		= (isset($result["Authority"]) 	&& $result["Authority"] != "") 	? $result["Authority"] : "";
-                $StartPay 		= (isset($result["Authority"]) 	&& $result["Authority"] != "") 	? "https://{$upay}.zarinpal.com/pg/StartPay/". $Authority : "";
-                $StartPayUrl 	= (isset($ZarinGate) 			&& $ZarinGate == true) 			? "{$StartPay}/ZarinGate" : $StartPay;
+                $Status = (isset($result["Status"]) && $result["Status"] != "") ? $result["Status"] : 0;
+                $Message = $this->error_message($Status, $Description, $CallbackURL, true);
+                $Authority = (isset($result["Authority"]) && $result["Authority"] != "") ? $result["Authority"] : "";
+                $StartPay = (isset($result["Authority"]) && $result["Authority"] != "") ? "https://{$upay}.zarinpal.com/pg/StartPay/" . $Authority : "";
+                $StartPayUrl = (isset($ZarinGate) && $ZarinGate == true) ? "{$StartPay}/ZarinGate" : $StartPay;
             }
 
             return array(
-                "Node" 		=> "{$node}",
-                "Method" 	=> "CURL",
-                "Status" 	=> $Status,
-                "Message" 	=> $Message,
-                "StartPay" 	=> $StartPayUrl,
+                "Node" => "{$node}",
+                "Method" => "CURL",
+                "Status" => $Status,
+                "Message" => $Message,
+                "StartPay" => $StartPayUrl,
                 "Authority" => $Authority
             );
         }
     }
 
-    public function verify($MerchantID, $Amount, $SandBox=false, $ZarinGate=false)
+    public function verify($MerchantID, $Amount, $SandBox = false, $ZarinGate = false)
     {
         $ZarinGate = ($SandBox == true) ? false : $ZarinGate;
 
-        if ($this->soap_check() === true)
-        {
-            $au 	= (isset($_GET['Authority']) && $_GET['Authority'] != "") ? $_GET['Authority'] : "";
-            $node 	= ($SandBox == true) ? "sandbox" : $this->zarinpal_node();
+        if ($this->soap_check() === true) {
+            $au = (isset($_GET['Authority']) && $_GET['Authority'] != "") ? $_GET['Authority'] : "";
+            $node = ($SandBox == true) ? "sandbox" : $this->zarinpal_node();
 
             $client = new SoapClient("https://{$node}.zarinpal.com/pg/services/WebGate/wsdl", ['encoding' => 'UTF-8']);
 
             $result = $client->PaymentVerification([
-                'MerchantID'     => $MerchantID,
-                'Authority'      => $au,
-                'Amount'         => $Amount,
+                'MerchantID' => $MerchantID,
+                'Authority' => $au,
+                'Amount' => $Amount,
             ]);
 
-            $Status 		= (isset($result->Status) 		&& $result->Status != "") 		? $result->Status 	: 0;
-            $RefID 			= (isset($result->RefID) 		&& $result->RefID != "") 		? $result->RefID 	: "";
-            $Message 		= $this->error_message($Status, "", "", false);
+            $Status = (isset($result->Status) && $result->Status != "") ? $result->Status : 0;
+            $RefID = (isset($result->RefID) && $result->RefID != "") ? $result->RefID : "";
+            $Message = $this->error_message($Status, "", "", false);
 
             return array(
-                "Node" 		=> "{$node}",
-                "Method" 	=> "SOAP",
-                "Status" 	=> $Status,
-                "Message" 	=> $Message,
-                "Amount" 	=> $Amount,
-                "RefID" 	=> $RefID,
+                "Node" => "{$node}",
+                "Method" => "SOAP",
+                "Status" => $Status,
+                "Message" => $Message,
+                "Amount" => $Amount,
+                "RefID" => $RefID,
                 "Authority" => $au
             );
         } else {
-            $au 	= (isset($_GET['Authority']) && $_GET['Authority'] != "") ? $_GET['Authority'] : "";
-            $node 	= ($SandBox == true) ? "sandbox" : "ir";
-            $upay 	= ($SandBox == true) ? "sandbox" : "www";
+            $au = (isset($_GET['Authority']) && $_GET['Authority'] != "") ? $_GET['Authority'] : "";
+            $node = ($SandBox == true) ? "sandbox" : "ir";
+            $upay = ($SandBox == true) ? "sandbox" : "www";
 
             $data = array('MerchantID' => $MerchantID, 'Authority' => $au, 'Amount' => $Amount);
             $jsonData = json_encode($data);
@@ -298,30 +288,29 @@ class PaymentController extends Controller
             curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: ' . strlen($jsonData)));
 
             $result = curl_exec($ch);
-            $err 	= curl_error($ch);
+            $err = curl_error($ch);
             curl_close($ch);
 
             $result = json_decode($result, true);
 
-            if ($err)
-            {
-                $Status 		= 0;
-                $Message 		= "cURL Error #:" . $err;
-                $Status 		= "";
-                $RefID 			= "";
+            if ($err) {
+                $Status = 0;
+                $Message = "cURL Error #:" . $err;
+                $Status = "";
+                $RefID = "";
             } else {
-                $Status 		= (isset($result["Status"]) && $result["Status"] != "") ? $result["Status"] : 0;
-                $RefID 			= (isset($result['RefID']) 	&& $result['RefID'] != "") 	? $result['RefID'] 	: "";
-                $Message 		= $this->error_message($Status, "", "", false);
+                $Status = (isset($result["Status"]) && $result["Status"] != "") ? $result["Status"] : 0;
+                $RefID = (isset($result['RefID']) && $result['RefID'] != "") ? $result['RefID'] : "";
+                $Message = $this->error_message($Status, "", "", false);
             }
 
             return array(
-                "Node" 		=> "{$node}",
-                "Method" 	=> "CURL",
-                "Status" 	=> $Status,
-                "Message" 	=> $Message,
-                "Amount" 	=> $Amount,
-                "RefID" 	=> $RefID,
+                "Node" => "{$node}",
+                "Method" => "CURL",
+                "Status" => $Status,
+                "Message" => $Message,
+                "Amount" => $Amount,
+                "RefID" => $RefID,
                 "Authority" => $au
             );
         }
