027df0e45ec897fa7b3ebfddbbab1369760f4ae9
AlirezaBelal <belal.alireza@yahoo.com>
2024-03-24 17:16:42 +0330

    SellerRegister mail to pass email data to the view

---
+++ app/Mail/SellerRegister.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Email;
+use App\Models\Seller;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class SellerRegister extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public $email;
+
+    public function __construct(Email $email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.seller.registration');
+    }
+}
